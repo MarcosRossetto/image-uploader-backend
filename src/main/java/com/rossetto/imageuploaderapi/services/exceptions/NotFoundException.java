a99502cb8c6aadec5bc51de5b87c56d989310d1b
Marcos Rossetto <marcosrst23@gmail.com>
2022-03-01 13:53:03 -0300

    feat: Classe de exceção NotFound

---
+++ src/main/java/com/rossetto/imageuploaderapi/services/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.rossetto.imageuploaderapi.services.exceptions;
+
+public class NotFoundException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+  
+  public NotFoundException(Long id) {
+    super("Resource not found: " + id);
+  }
+}
