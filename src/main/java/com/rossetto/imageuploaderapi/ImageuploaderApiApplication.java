cc7da964525713df329f03df4006286133a8ed51
Marcos Rossetto <marcosrst23@gmail.com>
2022-03-01 10:35:46 -0300

    Primeiro commit

---
+++ src/main/java/com/rossetto/imageuploaderapi/ImageuploaderApiApplication.java
@@ -0,0 +1,13 @@
+package com.rossetto.imageuploaderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageuploaderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageuploaderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rossetto/imageuploaderapi/ImageuploaderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rossetto.imageuploaderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageuploaderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
